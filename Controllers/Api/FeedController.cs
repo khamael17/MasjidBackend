6e2f68bdf8a17d7689a0ce9091c6a3427a596588
Khalilou Konate <KonateK@michigan.gov>
2023-03-05 20:26:51 -0500

    Post section almost done

---
+++ Controllers/Api/FeedController.cs
@@ -0,0 +1,34 @@
+﻿using Masjid.Models;
+using Masjid.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Masjid.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedController : ControllerBase
+    {
+        public IProfile _profile;
+
+        public FeedController( IProfile profile)
+        {
+            _profile = profile;
+            
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateFeed(Feed feed) {
+
+            if (feed.FeedType == "General Announcement") feed.FeedColor = "blue";
+            else if (feed.FeedType == "Request For Duaa") feed.FeedColor = "green";
+            else if (feed.FeedType == "Funeral") feed.FeedColor = "red";
+            else if (feed.FeedType == "Funds Raising") feed.FeedColor = "gold";
+
+            _profile.CreateFeed(feed);
+        
+        return new JsonResult(new { Success=true });
+        }
+    }
+}
---
+++ Controllers/Api/IslamicClassController.cs
@@ -0,0 +1,29 @@
+﻿using Masjid.Models;
+using Masjid.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Masjid.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IslamicClassController : ControllerBase
+    {
+        public IProfile _profile;
+
+
+
+        public IslamicClassController( IProfile profile )
+        {
+            _profile = profile;
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateIslamicClass(IslamicClasse model) {
+
+            _profile.CreateIslamicClass(model);
+            return new JsonResult(new {Success=true});
+        }
+    }
+}
